ec238dfd270fd6c2440844afb8c2e56eae43bf4d
Ghulam Rasool <seositesoft7@gmail.com>
2024-06-26 11:50:07 +0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,2 +1,2 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/usersdb");
+mongoose.connect("mongodb+srv://seositesoft7:seositesoft7@cluster0.hzqino1.mongodb.net/userbase-crud?retryWrites=true&w=majority&appName=Cluster0");
